5a3cb00727f22ca145449fdae641c7e95bc7a47c
Nicelozion <soloviov.mitia2017@yandex.ru>
2024-12-30 13:35:41 +0300

    added Hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
 import './App.css'
+import Hello from './components/Hello/Hello.jsx'
 
 function App() {
 
   return (
     <>
+      <div className="navbar">
+          <div className="navbar-links">
+            <ul>
+              <li href="#">Обо мне</li>
+              <li href="#">Хакатоны</li>
+              <li href="#">Планы</li>
+            </ul>
+          </div>
+      </div>
 
+      <Hello />
     </>
   )
 }
---
+++ src/components/Hello/Hello.jsx
@@ -0,0 +1,9 @@
+import "./Hello.css"
+
+export default function Hello() {
+    return(
+        <>
+            <h1>Привет</h1>
+        </>
+    )
+}
